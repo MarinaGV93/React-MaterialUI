d62482925ae4724fdbf9705694aaffc315b3db5b
MarinaGV93 <marinasgv@hotmail.com>
2024-05-08 15:14:21 -0300

    Layout base responsivo

--- src/shared/layouts/LayoutBaseDePagina.tsx
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -8,17 +8,22 @@ import {
   useTheme,
 } from "@mui/material";
 import { useDrawerContext } from "../contexts";
+import { ReactNode } from "react";
 
 interface ILayoutBaseDePaginaProps {
   children: React.ReactNode;
   titulo: string;
+  barraDeFerramentas?: ReactNode;
 }
 
 export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({
   children,
   titulo,
+  barraDeFerramentas,
 }) => {
   const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down("sm"));
+
+  const mdDown = useMediaQuery((theme: Theme) => theme.breakpoints.down("md"));
 
   const theme = useTheme();
 
@@ -36,8 +41,8 @@ export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({
         // Centralizar verticalmente
         alignItems="center"
         padding={1}
-        height={theme.spacing(12)}
         gap={1}
+        height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)}
       >
         {/* Menu lateral (hamburguer) */}
         {/* Se a tela for menor que sm */}
@@ -49,12 +54,26 @@ export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({
         {/* variant = h1, h2, ..., subtitle1,...
             component = igual do HTML. Não muda na estilização, somente para semantica
         */}
-        <Typography variant="h5" component="b">
+        <Typography
+          component="b"
+          // Se o texto tiver grande, não deixa que passe o elemento, cortando o texto (desaparecer)
+          overflow="hidden"
+          // Não quebrar linha
+          whiteSpace="nowrap"
+          // Que apareça '...' no final do texto
+          textOverflow="ellipsis"
+          variant={smDown ? "h5" : mdDown ? "h4" : "h3"}
+        >
           {titulo}
         </Typography>
       </Box>
-      <Box>Barra de ferramentas</Box>
-      <Box>{children}</Box>
+
+      {barraDeFerramentas && <Box>{barraDeFerramentas}</Box>}
+
+      {/* overFlow = permitir que quando o children tiver uma altura muito grande com muitos componentes filhos, permite que a div (box) tenha scroll */}
+      <Box flex={1} overflow="auto">
+        {children}
+      </Box>
     </Box>
   );
 };
